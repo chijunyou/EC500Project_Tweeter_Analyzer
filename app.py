e130f0ab8ae53bb8ac278047d45985ae853259de
hanchenzhang <43228680+hanchenzhang@users.noreply.github.com>
2020-06-02 15:17:23 +0800

    Update app.py

--- app.py
+++ app.py
@@ -1,6 +1,7 @@
 from flask import Flask, render_template, request, jsonify, Markup
 import flask_restful
 from dataprocess import bothpro
+from dataprocesschina import twopro
 app = Flask(__name__)
 app.config["DEBUG"] = True
 
@@ -23,10 +24,23 @@ def search():
         bothpro(keyword)
         return render_template('choice.html')
 
+@app.route('/search_in_china/',methods=['GET','POST'])
+def search_in_china():
+    if request.method == 'GET':
+        return render_template('search_in_china.html')
+    else:
+        text = request.form["text"]    # get form text
+        keyword = text.upper()  # process form text
+        twopro(keyword)
+        return render_template('choice.html')
 
 @app.route('/geolocation/',methods=['GET'])
 def showheatmap():
     return render_template("heatmap.html")
+
+@app.route('/baidugeolocation/',methods=['GET'])
+def showbaiduheatmap():
+    return render_template("baiduheatmap.html")
 
 @app.route('/chart/',methods=['GET'])
 def showchart():
